bf9b783d0736344e9df15755b3c70f95572abaf5
Amrutha <96amruthagopinath@gmail.com>
2018-03-24 12:52:08 +0530

    done

--- Server.java
+++ Server.java
@@ -9,3 +9,45 @@ public class Server
 		{
 			server = new ServerSocket(portnum);
 		}
+catch (Exception err)
+		{
+			System.out.println(err);
+		}
+	}
+
+	public void serve()
+	{
+		try
+		{
+			while (true)
+			{
+				Socket client = server.accept();
+				BufferedReader r = new BufferedReader(new InputStreamReader(client.getInputStream()));
+				PrintWriter w = new PrintWriter(client.getOutputStream(), true);
+				w.println("Welcome to the Java EchoServer.  Type 'bye' to close.");
+				String line;
+				do
+				{
+					line = r.readLine();
+					if ( line != null ){
+						w.println("Server: "+ line);			
+					}
+				}
+				while ( !line.trim().equals("bye") );
+				client.close();
+			}
+		}
+		catch (Exception err)
+		{
+			System.err.println(err);
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		EchoServer s = new EchoServer(9999);
+		s.serve();
+	}
+
+	private ServerSocket server;
+}
